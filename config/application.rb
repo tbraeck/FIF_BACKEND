5213c6daf44cde0c14abc5120b06dfbfab65abb9
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-10-16 14:35:11 -0600

    ADjustments to views for BE

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,15 @@ module FifBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+       # CORS configuration
+       config.middleware.insert_before 0, Rack::Cors do
+        allow do
+          origins 'http://localhost:3001' # You can restrict this to specific origins, e.g., 'http://localhost:3001'
+          resource '*',
+            headers: :any,
+            methods: [:get, :post, :put, :patch, :delete, :options, :head],
+            credentials: true
+        end
+      end
   end
 end
